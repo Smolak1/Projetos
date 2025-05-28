4d19965b50e7d7f7f6c648e8d97c43466dfe65e9
DEV de Rondônia <jcsmolakr@gmail.com>
2025-05-28 12:20:39 -0400

    Criado bhaskara.js

---
+++ bhaskara.js
@@ -0,0 +1,16 @@
+import input from 'readline-sync';
+
+let a = input.questionInt(`A: `)
+let b = input.questionInt(`B: `)
+let c = input.questionInt(`C: `)
+
+let bNegativo = b * -1
+let bQuadrado = Math.pow(b, 2)
+let calc2P = -4 * a * c
+let calc1P = bQuadrado + calc2P
+let divisao = 2 * a
+let calc3 = Math.sqrt(calc1P)
+let calcX = (bNegativo + calc3) / divisao
+let calcY = (bNegativo - calc3) / divisao
+
+console.log(`X = ${calcX} X' ${calcY}`)
